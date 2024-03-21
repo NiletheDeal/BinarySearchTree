d93f07ed4f532ca90ac32b3880a3dfa691a7c4fb
U-BSD\420607 <420607@SHS-GYBF6X3.beaverton.k12.or.us>
2024-03-21 12:23:54 -0700

    Done with Search, ADD, PRINT, need to work on delete

--- main.cpp
+++ main.cpp
@@ -3,9 +3,13 @@
 #include <cctype>
 #include "node.h"
 
-int main {
+void ADD(Node*& root, Node* newNode, Node* tree);
+void print(Node* tree, int count);
+bool search(Node* tree, int s);
+int main(){
   cout << "Welcome to BST" << endl << endl;
   bool looping = true;
+  bool adding = true;
   char command[10];
   char input[3200];
   int value = -1;
@@ -16,13 +20,95 @@ int main {
     cin.clear();
     cin.ignore(10000, '\n');
     if(strcmp(command, "ADD") == 0) {
-      cout << "Enter you input of numbers seperated by spaces" << endl;
-      while(cin >> value) { 
-	Node* temp = new Node();
-	temp->setData(value);
-	ADD(tree, temp);
+      adding = true;
+      cout << "Enter you input of numbers seperated by spaces and put a -1 at end of the list" << endl;
+      while(adding == true) {
+	cin >> value;
+	if(value == -1) {
+	  adding = false;
+	}
+	else {
+	  Node* temp = new Node();
+	  temp->setData(value);
+	  ADD(tree, temp, tree);
+	}
+      }
+      cin.clear();
+      cin.ignore(10000, '\n');
+    }
+    else if(strcmp(command, "SEARCH") == 0) {
+      cout << "Enter the number you are searching " << endl;
+      int searchInt;
+      cin >> searchInt;
+      cin.clear();
+      cin.ignore(10000, '\n');
+      bool inTree = search(tree, searchInt);
+      if(inTree == true) {
+	cout << "That number is in the tree" << endl;
+      }
+      else {
+	cout << "That number isn't in the tree" << endl;
+      }
+    }
+    else if(strcmp(command, "PRINT") == 0) {
+      print(tree, 0);
+    }
+  }
+}
+void ADD(Node*& root, Node* newNode, Node* tree ) {
+  
+  if(root == NULL) {
+    root = newNode;
+  }
+  else {
+    int treeD = tree->getData();
+    int addD = newNode->getData();
+    if(treeD > addD) {
+      if(tree->getLeft() == NULL) {
+	tree->setLeft(newNode);
+      }
+      else {
+	ADD(root, newNode, tree->getLeft());
+      }
+    }
+    else if(treeD <= addD) {
+      if(tree->getRight() == NULL) {
+	tree->setRight(newNode);
+      }
+      else {
+	ADD(root, newNode, tree->getRight());
       }
     }
   }
 }
-void ADD(Node* tree, Node* newNode);
+void print(Node* tree, int count) {//print out tree
+  if(tree->getRight() != NULL) {//get to the right child
+    print(tree->getRight(), count+1);
+  }
+  for(int i = 0; i < count; i++) {//tabs over
+    cout << '\t';
+  }
+  cout << tree->getData() << endl;//print out value
+  //left side
+  if(tree->getLeft() != NULL) {//get to the left child
+    print(tree->getLeft(), count+1);
+  }
+}
+bool search(Node* tree, int s) {
+  if(tree == NULL) {
+    return false;
+  }
+  else {
+    int treeD = tree->getData();
+    if(treeD == s) {
+      return true;
+    }
+    else if(treeD > s) {
+      return search(tree->getLeft(), s);
+    }
+    else if(treeD < s) {
+      return search(tree->getRight(), s);
+    }
+  }
+  return false;
+}
